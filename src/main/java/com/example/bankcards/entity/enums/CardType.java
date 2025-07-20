49523671b3e68e4a8a1965403a4c59ee93716345
AnsySan <epaskevic4@gmail.com>
2025-07-20 20:33:05 +0300

    refactoring

---
+++ src/main/java/com/example/bankcards/entity/enums/CardType.java
@@ -0,0 +1,28 @@
+package com.example.bankcards.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum CardType {
+    VISA(1, "Visa", "411111", "4[0-9]{12,15}", 16),
+
+    MASTERCARD(2, "MasterCard", "511111", "5[1-5][0-9]{14}", 16);
+
+
+    public static final String MASKED_CARD_PATTERN = "^(\\*{4} ?)*\\d{4}$";
+    public static final int DIGIT_IN_ONE_SECTION = 4;
+
+    private final int typeCode;
+    private final String name;
+    private final String prefix;
+    private final String regex;
+    private final int length;
+
+    CardType(int typeCode, String name, String prefix, String regex, int length) {
+        this.typeCode = typeCode;
+        this.name = name;
+        this.prefix = prefix;
+        this.regex = regex;
+        this.length = length;
+    }
+}
