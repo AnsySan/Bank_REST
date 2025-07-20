8277e3ad0956984023c954722496ea88e5d83f38
AnsySan <epaskevic4@gmail.com>
2025-07-20 13:53:31 +0300

    adder @Repository

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -3,10 +3,12 @@ package com.example.bankcards.repository;
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.entity.CardStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findByCardStatus(CardStatus cardStatus);
