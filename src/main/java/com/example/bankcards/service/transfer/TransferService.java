3991ef1ce8ec97f85249d6a6af494758482f8f28
AnsySan <epaskevic4@gmail.com>
2025-07-23 13:17:59 +0300

    init

--- src/main/java/com/example/bankcards/service/transfer/TransferService.java
+++ src/main/java/com/example/bankcards/service/transfer/TransferService.java
@@ -1,8 +1,6 @@
 package com.example.bankcards.service.transfer;
 
-import com.example.bankcards.dto.request.TransferRequest;
 import com.example.bankcards.dto.response.TransferResponse;
-import com.example.bankcards.entity.Transfer;
 
 public interface TransferService {
     TransferResponse transfer(TransferResponse transferResponse);
